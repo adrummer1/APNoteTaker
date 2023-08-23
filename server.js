1e11a06a3495b3b467f095500687f25b39890f31
adrummer1 <andrew@bluestemcreativeco.com>
2023-08-23 14:43:44 -0500

    Removed store.js.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require('path');
 const api = require('./routes/apiroutes.js');
 const html = require('./routes/htmlroutes.js');
-const store = require('./helpers/store');
 
 const PORT = process.env.PORT || 3001;
 
